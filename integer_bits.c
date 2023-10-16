d5ab4e0387e8298f3180d693926e868d3b361589
darthjee <darthjee@gmail.com>
2023-10-16 07:15:41 -0300

    Show table

--- integer_bits.c
+++ integer_bits.c
@@ -7,13 +7,36 @@
 
 char in(char opt, char *options);
 char read_option(char *options, char* text);
+void show_table(int number);
+char *bit_string(unsigned char value);
 
 int main() {
   char exit;
+  int number;
 
   do {
+    show_table(number);
     exit = read_option("ynYN", "\nDo you wanna quit? [y/n]");
   } while(! in(exit, "yY"));
+}
+
+void show_table(int number) {
+  int i;
+  char *array = (char *) &number;
+  char sig;
+  unsigned char not_sig;
+
+  printf("Number: %d\n", number);
+  printf("index\tsigned\tunsigned\n");
+
+  for (i = 0; i < 4; i++) {
+    sig = not_sig = array[i];
+
+    printf("[%d]\t%+04d\t%03d\n", i, sig, not_sig);
+  }
+}
+
+char *bit_string(unsigned char value) {
 }
 
 char read_option(char *options, char* text) {
