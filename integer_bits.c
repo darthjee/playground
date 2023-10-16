1bff7e5c1272c867656b36749e1388f5b6ddb3e9
darthjee <darthjee@gmail.com>
2023-10-16 07:42:16 -0300

    Display bits

--- integer_bits.c
+++ integer_bits.c
@@ -9,6 +9,7 @@ char in(char opt, char *options);
 char read_option(char *options, char* text);
 void show_table(int number);
 char *bit_string(unsigned char value);
+char *reverse_string(char *value);
 
 int main() {
   char exit;
@@ -23,20 +24,50 @@ int main() {
 void show_table(int number) {
   int i;
   char *array = (char *) &number;
+  char *bits, *reverse_bits;
   char sig;
   unsigned char not_sig;
 
   printf("Number: %d\n", number);
-  printf("index\tsigned\tunsigned\n");
+  printf("index\tsigned\tunsigned\tbit\t\tinverted\n");
 
   for (i = 0; i < 4; i++) {
     sig = not_sig = array[i];
+    bits = bit_string(not_sig);
+    reverse_bits = reverse_string(bits);
 
-    printf("[%d]\t%+04d\t%03d\n", i, sig, not_sig);
+    printf("[%d]\t%+04d\t%03d\t\t%s\t%s\n", i, sig, not_sig, bits, reverse_bits);
   }
 }
 
+char *reverse_string(char *string) {
+  int len, i;
+  char *reverse;
+
+  reverse = malloc((len + 1) * sizeof(char));
+  len = strlen(string);
+  reverse[8] = '\0';
+
+  for (i = 0; i < len; i++) {
+    reverse[i] = string[len - i - 1];
+  }
+
+  return reverse;
+}
+
 char *bit_string(unsigned char value) {
+  char *string;
+  char i;
+
+  string = malloc(9 * sizeof(char));
+  string[8] = '\0';
+
+  for(i = 0; i < 8; i++) {
+    string[i] = '0' + value % 2;
+    value /= 2;
+  }
+
+  return string;
 }
 
 char read_option(char *options, char* text) {
